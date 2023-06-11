f162269d22ad82d81573aa1055f2446bb9088304
E_Kazak <kazak-zhenya2017@mail.ru>
2023-06-11 14:24:17 +0300

    Первый коммит.

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App() {
+  
+
+  return (
+    <View>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
